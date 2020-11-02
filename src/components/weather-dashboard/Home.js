ba4bb3c43b8552e26ff6a9fa14c22ba85e4e49d9
AvijeetDey96 <avijeetdey96@gmail.com>
2020-11-02 17:23:12 +0530

    Make slider

--- src/components/weather-dashboard/Home.js
+++ src/components/weather-dashboard/Home.js
@@ -7,47 +7,72 @@ import Errorscreen from './error-screen/Errorscreen'
 import useInpuState from '../../Hooks/useInputState'
 const api = {
     key: '925eb958bfa1d5211e1f269ee689cfef',
-    base: 'https://api.openweathermap.org/data/2.5/weather',
-    corsUrl: 'https://cors-anywhere.herokuapp.com'
+    base: 'https://api.openweathermap.org/data/2.5/weather', 
+    daysCall: 'https://api.openweathermap.org/data/2.5/forecast',
+    corsUrl: 'https://cors-anywhere.herokuapp.com',
+
 }
+  
 function Home(props) {
     const [value, handleChange, reset] = useInpuState("");
     const [weather, setWeather] = useState({})
+    const [allWeather, setAllWeather] = useState({})
 
     const search = evt => {
         evt.preventDefault();
+        console.log('zxffg')
         if (evt.key == "Enter") {
             // let url = `${api.base}?q=${value}&appid=${api.key}`
-              let url = `${api.corsUrl}/${api.base}?q=${value}&appid=${api.key}`
+            let url = `${api.corsUrl}/${api.base}?q=${value}&appid=${api.key}`
             // const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-            fetch(( url), {
+            fetch((url), {
                 crossDomain: true,
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                   'Access-Control-Allow-Origin':'*'
+                    'Access-Control-Allow-Origin': '*'
                 }
             })
                 .then(res => {
-                   return res.json();  
+                    return res.json();
                 })
                 .then(result => {
                     console.log('weather', result)
-
                     setWeather(result);
-                    reset();
+                    if(typeof result.coord !== 'undefined') {
+                        searchAllData(result.coord);
+                    }
+                   
                 })
         }
-
-
-   
+    }
+    function searchAllData( coord){
+          let url = `${api.corsUrl}/${api.daysCall}?q=${value}&appid=${api.key}`
+    //    let url = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall?lat=26.7&lon=88.43&exclude=minutely&appid=925eb958bfa1d5211e1f269ee689cfef"
+        fetch((url), {
+            crossDomain: true,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(result => {
+                 setAllWeather(result);
+                reset();
+            })
+       
     }
     return (
         <>
-            <SearchAppBar   onChange={handleChange} value={value} searchable={search}></SearchAppBar>
-        { typeof weather.main !=='undefined' && weather.cod === 200?<Datascreen data={weather} value={value}></Datascreen> :<Errorscreen></Errorscreen>} 
-            
+        <div style={{backgroundAttachment:"fixed"}}> <SearchAppBar onChange={handleChange} value={value} searchable={search}></SearchAppBar> </div>
+           
+            { typeof weather.main !== 'undefined' && weather.cod === 200 ? <Datascreen alldatas={allWeather} data={weather} value={value}></Datascreen> : <Errorscreen></Errorscreen>}
+
         </>
     );
 }
--- src/components/weather-dashboard/data-screen/Datascreen.js
+++ src/components/weather-dashboard/data-screen/Datascreen.js
@@ -29,23 +29,62 @@ function meterToKilometer(met) {
 function mtPerSecToKmHr(mtPerSec) {
     return Math.ceil(mtPerSec * (18 / 5))
 }
+function urlMaker(icon) {
+    let url = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    return url
+}
 
+function getWeekDay(dt) {
+     
+    var d = new Date(dt);
+
+    var weekday = new Array(7);
+
+    weekday[0] = "Sun";
+
+    weekday[1] = "Mon";
+
+    weekday[2] = "Tue";
+
+    weekday[3] = "Wed";
+
+    weekday[4] = "Thur";
+
+    weekday[5] = "Fri";
+
+    weekday[6] = "Sat";
+
+    
+    let n = weekday[d.getDay()];
+    return n  
+    
+}
+function getDay(d) { 
+    let rawDate = new Date(d)
+    let date= rawDate.getDate()
+   let day = getWeekDay(d)
+   console.log(date,day);
+   return (<span>{day}&nbsp;{date}</span>)
+   
+}
 function Datascreen(props) {
 
     const weatheData = props.data;
+    const allInfoData = props.alldatas;
+    const hourlyData = allInfoData.hourly;
+    const dailyData = allInfoData.list;
+    console.log('dailyData', dailyData)
     let { base, clouds, coord, main, name, sys, timezone, visibility, weather, wind } = weatheData;
-    let temp = kelvinToCelsius(main.temp)
-    let temp_min = kelvinToCelsius(main.temp_min)
     let imageUrl = "http://openweathermap.org/img/wn/" + weather[0].icon + "@2x.png"
     const classes = useStyles();
 
     return (
         <>
-            <div className={classes.root}>
+            <div className={classes.root} className="background-cover night">
 
-                <Grid container spacing={3} className="background-cover night">
+                <Grid container   >
 
-                    <Grid item xs={12} >
+                    <Grid xs={12} >
                         <Grid container className="font">
 
                             <Grid item xs={4}> </Grid>
@@ -55,38 +94,72 @@ function Datascreen(props) {
                             <Grid item xs={4}>  </Grid>
                             <Grid item xs={5}>  </Grid>
                             <Grid item xs={1}> <img src={imageUrl} /></Grid>
-                            <Grid item xs={1}><span style={{ fontSize: "60px" }}>{temp}° </span>  C</Grid>
+                            <Grid item xs={1}><span style={{ fontSize: "60px" }}>{kelvinToCelsius(main.temp)}° </span>  C</Grid>
                             <Grid item xs={5}>  </Grid>
                             <Grid item xs={5}>  </Grid>
-                            <Grid item xs={2} style={{ fontSize: "25px",lineHeight: "0" }}><span>{weather[0].description}</span>  </Grid>
+                            <Grid item xs={2} style={{ fontSize: "25px", lineHeight: "0" }}><span>{weather[0].description}</span>  </Grid>
                             <Grid item xs={5}>  </Grid>
                             <Grid item xs={4}>  </Grid>
-                            <Grid item xs={4} style={{ marginTop: "21px"}}>
+                            <Grid item xs={4} style={{ marginTop: "21px" }}>
                                 <Grid container spacing={1} className="details">
                                     <Grid item xs={4}> <span>Feels like &nbsp; {kelvinToCelsius(main.feels_like)} °C</span> </Grid>
                                     <Grid item xs={4}> <span>Wind speed  &nbsp;  {mtPerSecToKmHr(wind.speed)} km/h</span> </Grid>
                                     <Grid item xs={4}><span>Visibility   &nbsp; {meterToKilometer(visibility)} km </span> </Grid>
                                     <Grid item xs={4}>  <span>Humidity  &nbsp; {main.humidity}   % </span> </Grid>
                                     <Grid item xs={4}> <span>Pressure  &nbsp; {main.pressure}  hPa </span> </Grid>
-                                    <Grid item xs={4}> <span>Sea Level  &nbsp; {main.sea_level}  hPa </span> </Grid>
+                                    <Grid item xs={4}> <span>Sea Level  &nbsp; {main.sea_level ? main.sea_level : 0}  hPa </span> </Grid>
                                 </Grid>
                             </Grid>
                             <Grid item xs={4}>  </Grid>
 
                         </Grid>
-                        {/* <div className="font">
-                        
-                           
-                             <br />
-                            <br />
-                             <br />
-                            <br />
-                            <span>grnd_level {main.grnd_level}  hPa </span> <br />
-                        </div> */}
 
                     </Grid>
 
                 </Grid>
+
+
+                {/* <div className="font" style={{ float: "left", padding: "2%" }}>Daily</div>
+
+                <div className="container-1">
+                    {typeof dailyData !== 'undefined' && dailyData.length > 0 ? dailyData.map((data, index) =>
+                   
+                    (
+                       
+                         <div className="flex-box" key={index}>
+                                <div className="box">
+                                    <div>Mon 2</div>
+                                     
+                                    <img className="smallImage" src={urlMaker(data.weather[0].icon)} />
+                                </div>
+
+                            </div>
+                       
+                    ) ) : 'No Data Available'}
+
+                 </div> */}
+                <br />
+
+                <div className="font" style={{ float: "left", padding: "2%" }}>Daily and Hourly</div>
+               
+                <br/>
+
+                <div className="container-1 scrolling-wrapper">
+                    {typeof dailyData != 'undefined' && dailyData.length > 0 ? dailyData.map((data, index) => (
+                        <div className="flex-box wrapper">
+                            <div className="box">
+                        <div>{getDay(data.dt_txt)}</div>
+                            <img className="smallImage" src={urlMaker(data.weather[0].icon)} /> <br/>
+                             <div> <span style={{    fontSize: "28px",fontWeight: 400}}>{kelvinToCelsius(data.main.temp_max)}°</span>&nbsp;<span>{kelvinToCelsius(data.main.temp_min)}°</span></div> 
+                            <div>
+                                {data.weather[0].description}
+                            </div>
+                            </div>
+
+                        </div>
+                    )) : ''}
+
+                </div>
 
             </div></>
 
