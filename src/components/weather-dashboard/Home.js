bfa1bbbe425cc0e70114a07eb69f89edaae2cd8f
AvijeetDey96 <avijeetdey96@gmail.com>
2020-11-05 18:35:25 +0530

    change fetch to axioms

--- src/components/weather-dashboard/Home.js
+++ src/components/weather-dashboard/Home.js
@@ -1,114 +1,98 @@
-import React, { useState } from 'react';
- import SearchAppBar from '../sections/Navbar'
+import React, { useState,useEffect } from 'react';
+import SearchAppBar from '../sections/Navbar'
 import Datascreen from './data-screen/Datascreen'
 import Errorscreen from './error-screen/Errorscreen'
 
 import useInpuState from '../../Hooks/useInputState'
+import axios from "axios";
+
+let geoWeather = 'aaa';
 const api = {
     key: 'a03ddfcfde2a4376a2d6d014ec9fbf6c',
     base: 'https://api.openweathermap.org/data/2.5/weather',
     daysCall: 'https://api.openweathermap.org/data/2.5/forecast',
-    corsUrl: 'https://cors-anywhere.herokuapp.com',
+    // corsUrl: 'https://cors-anywhere.herokuapp.com',
 
 }
 
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
 
+ 
 function Home(props) {
     const [value, handleChange, reset] = useInpuState("");
     const [weather, setWeather] = useState({})
     const [allWeather, setAllWeather] = useState({})
-
-
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
+//     function success(pos) {
+//         var crd = pos.coords;
+//          getLocationUsinfLonLat(crd.longitude, crd.latitude)
+//     }
     
-    function success(pos) {
-        var crd = pos.coords;
+//     function error(err) {
+//         console.warn(`ERROR(${err.code}): ${err.message}`);
+//     }
     
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
-        getLocationUsinfLonLat(crd.longitude, crd.latitude)
-    }
     
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-    const  getLocationUsinfLonLat = (lon, lat)=> {
-        let url = `${api.corsUrl}/${api.base}?lat=${lat}&lon=${lon}&appid=${api.key}`
-        fetch((url), {
-            crossDomain: true,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        })
-            .then(res => {
-                return res.json();
-            })
-            .then(result => {
-                console.log('weather', result)
-                setWeather(result);
-                
-                    searchAllData();
-                
-    
-            })
-    }
+//      function getGeo(){
+//         navigator.geolocation.getCurrentPosition(success, error, options);
+//      }
+//     const getLocationUsinfLonLat = (lon, lat) => {
+//         let url = `${api.base}?lat=${lat}&lon=${lon}&appid=${api.key}`
+   
+//        axios.get(url)
+//            .then(function (res) {
+//                let result = res.data;
+//                geoWeather = result;
+//                return result;
+//            })
+//            .catch(function (error) {
+//                // handle error
+//                console.log(error);
+//            })
+//    }
+//    getGeo();
+    useEffect(()=>{
+        
+    })
 
     const search = evt => {
         evt.preventDefault();
         if (evt.key === "Enter") {
-            // let url = `${api.base}?q=${value}&appid=${api.key}`
-            let url = `${api.corsUrl}/${api.base}?q=${value}&appid=${api.key}`
-            // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+            let url = `${api.base}?q=${value}&appid=${api.key}`
             console.log(url);
-            fetch((url), {
-                crossDomain: true,
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }
-            })
-                .then(res => {
-                    return res.json();
-                })
-                .then(result => {
-                    console.log('weather', result)
-                    setWeather(result);
-                    
-                        searchAllData();
-                     
+            axios.get(url)
+                .then(function (res) {
 
+                    let result = res.data;
+                    setWeather(result);
+                    searchAllData();
                 })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                    let result ={message:'city not found'}
+                    setWeather(result);
+                })
+
         }
     }
     function searchAllData() {
-        let url = `${api.corsUrl}/${api.daysCall}?q=${value}&appid=${api.key}`
-        //    let url = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall?lat=26.7&lon=88.43&exclude=minutely&appid=925eb958bfa1d5211e1f269ee689cfef"
-        fetch((url), {
-            crossDomain: true,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        })
-            .then(res => {
-                return res.json();
-            })
-            .then(result => {
+        let url = `${api.daysCall}?q=${value}&appid=${api.key}`
+
+        axios.get(url)
+            .then(function (res) {
+                console.log('weather', res.data)
+                let result = res.data;
                 setAllWeather(result);
                 reset();
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
             })
 
     }
