5f4550c77a7c54879e7a94b71e9dbed939dbcda9
AvijeetDey96 <avijeetdey96@gmail.com>
2020-11-05 20:20:46 +0530

    height

--- src/components/weather-dashboard/data-screen/Datascreen.js
+++ src/components/weather-dashboard/data-screen/Datascreen.js
@@ -8,7 +8,7 @@ import sunset from '../../../../src/assets/icon/sunset.svg';
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-    },
+    }, 
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
